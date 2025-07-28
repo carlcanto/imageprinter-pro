c6596f33904454aea1738ce6fca9c9212418c5fb
carlcanto <carloscanto04@gmail.com>
2025-07-28 17:37:04 -0500

    prueba1 para ver funcionamiento de repositorio

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error('❌ Error conectando a MongoDB:', err));
 
 app.get('/', (req, res) => {
-  res.send('✅ Backend funcionando correctamente');
+  res.send('✅ ivan todo funciona bien');
 });
 
 app.listen(port, () => {
