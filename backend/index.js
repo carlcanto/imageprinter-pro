c60a769bfbe6da7a000bec9cfb8b2614e5e2b6c7
carlcanto <carloscanto04@gmail.com>
2025-08-25 21:50:10 -0500

    porfin backend y frontend conectados

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,17 @@
 // backend/index.js
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // Permitir peticiones desde el frontend
 
 app.get("/hello", (req, res) => {
   res.json({ message: "¡Hola desde el backend!" });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
